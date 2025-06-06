293e642d9c34210375d215cb051a078e7df6bef9
kenny268 <kentmutai5@gmail.com>
2025-06-07 02:32:10 +0300

    Created a reusable types for the project

---
+++ app/types/skip.ts
@@ -0,0 +1,10 @@
+// app/types/skip.ts
+export interface Skip {
+  id: number;
+  size: number;
+  hire_period_days: number;
+  price_before_vat: number;
+  vat: number;
+  allowed_on_road: boolean;
+  allows_heavy_waste: boolean;
+}
